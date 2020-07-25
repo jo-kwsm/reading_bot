13218bae2b333977f4e1d676f2f1ac5b5d6b4abb
jo-kwsm <ytk.kwsm@gmail.com>
2020-07-26 03:45:48 +0900

    鋳型ファイルの作成

--- src/bot.py
+++ src/bot.py
@@ -1,1 +1,29 @@
 import discord
+from discord.ext import commands
+import subprocess
+#import ffmpeg
+#from voice_generator import creat_WAV
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
+        else :
+            m="おはよう"
+            await message.channel.send(m)
+
+client.run("NzM2NjE2Mzk4NTk1NTU1Mzg2.XxxZew.TRTU4YfKA1f-XMvFkqUBuEDBP-Q")
